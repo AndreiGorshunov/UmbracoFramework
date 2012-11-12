bc78e71ab25d26b6de44915793ab58c23dfb01ed
agorshunov <andrei.gorshunov@gmail.com>
2012-11-13 00:45:12 +0300

    Initial commit

---
+++ UmbracoFramework/Utils/LinkManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using umbraco.MacroEngines;
+using umbraco.MacroEngines.Library;
+using umbraco.NodeFactory;
+
+namespace UmbracoFramework.Utils
+{
+    public static class LinkManager
+    {
+        public static string GetLinkUrl(dynamic linkProperty, out bool isInternal)
+        {
+            dynamic link = linkProperty.BaseElement.Element("link");
+            if (link.Attribute("type").Value == "internal")
+            {
+                int relatedNodeId = int.Parse(link.Attribute("link").Value);
+                dynamic relatedNode = new RazorLibraryCore(Node.GetCurrent()).NodeById(relatedNodeId);
+                isInternal = true;
+                return relatedNode.Url;
+            }
+
+            isInternal = false;
+            return link.Attribute("link").Value;
+        }
+
+        public static dynamic GetLinkedNode(dynamic linkProperty)
+        {
+            dynamic link = linkProperty.BaseElement.Element("link");
+            if (link.Attribute("type").Value == "internal")
+            {
+                int relatedNodeId = int.Parse(link.Attribute("link").Value);
+                return new RazorLibraryCore(Node.GetCurrent()).NodeById(relatedNodeId);
+            }
+
+            return null;
+        }
+    }
+}
