fa0aadf3962b43a56fb321d24a9e1d1182a9a0c4
agorshunov <andrei.gorshunov@gmail.com>
2012-11-29 12:03:07 +0300

    Logging for Umbraco

---
+++ UmbracoFramework/Diagnostics/Log.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using log4net;
+
+namespace UmbracoFramework.Diagnostics
+{
+    public static class Log
+    {
+        public static void Error(Type type, Exception ex, string message = null)
+        {
+            ILog log = LogManager.GetLogger(type);
+            log.Error(message, ex);
+        }
+    }
+}
---
+++ UmbracoFramework/Diagnostics/Logger.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using log4net;
+using log4net.Config;
+
+namespace UmbracoFramework.Diagnostics
+{
+    public class Logger : System.Web.HttpApplication
+    {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(Logger));
+
+        public Logger()
+        {
+            InitializeComponent();
+        }
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            // Application start-up code goes here.
+            XmlConfigurator.Configure();
+
+            Log.Debug("Fired up logger in application_start");
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+            Log.Debug("closed logger in application_end");
+            LogManager.Shutdown();
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            // Code that runs when an unhandled error occurs
+
+            Log.Error("Unhandled exception", Context.Error);
+        }
+
+        private void InitializeComponent()
+        {
+
+        }
+    }
+}
