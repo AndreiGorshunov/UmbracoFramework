dc9f3105c35d25c5c89b5c951c720eeef3b5526d
Andrei Gorshunov <andrei.gorshunov@gmail.com>
2013-01-06 17:05:48 +0300

    CartModel

---
+++ SHBusinessLogic/Forms/CartModel.cs
@@ -0,0 +1,30 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="CartModel.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace SHBusinessLogic.Forms
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class CartModel
+    {
+        public static string SessionKey = "SH.Form.Cart";
+
+        public CartModel()
+        {
+            this.ItemIds = new List<int>();
+        }
+
+        public List<int> ItemIds { get; private set; }
+
+        public string BackUrl { get; set; }
+    }
+}
